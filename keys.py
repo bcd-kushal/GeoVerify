4e6841f91583e97f1d75df728d9412acf72c4768
Kushal <dev@kushalkumarsaha.com>
2024-02-12 00:58:34 +0530

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,56 @@
+KEYS = {
+    "FROM": [
+        "CITIES",
+        "COUNTRIES",
+        "REGIONS",
+        "STATES",
+        "SUBREGIONS"
+    ],
+    "CITIES": [ 
+        "id",
+        "name",
+        "state_id",
+        "state_code",
+        "state_name",
+        "country_id",
+        "country_code",
+        "country_name",
+        "latitude",
+        "longitude"
+     ],
+    "COUNTRIES": [ 
+        "id",
+        "name",
+        "iso3",
+        "iso2",
+        "numeric_code",
+        "phone_code",
+        "capital",
+        "currency",
+        "currency_name",
+        "region",
+        "subregion",
+        "nationality",
+        "latitude",
+        "longitude",
+     ],
+    "REGIONS": [ 
+        "id",
+        "name",
+     ],
+    "STATES": [ 
+        "id",
+        "name",
+        "country_id",
+        "country_code",
+        "country_name",
+        "state_code",
+        "latitude",
+        "longitude"
+     ],
+    "SUBREGIONS": [ 
+        "id",
+        "name",
+        "region_id",
+     ],
+}
